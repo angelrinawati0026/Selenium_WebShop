7b6bf3adf37bd3dbad2836a03ef730b6d9ff4df5
Angeli <26angelsilaban@gmail.com>
2024-05-24 23:40:14 +0700

    web automation using selenium python

--- pageObject/checkoutPage.py
+++ pageObject/checkoutPage.py
@@ -1,7 +1,13 @@
 class CheckoutPage():
-    category_books = "xpath ://div[@class='listbox']/ul/li/a[text()='Books']"
-    url_books = 'https://demowebshop.tricentis.com/books'
+    img_produk = "div[class='product-item'] img[title='Show details for $25 Virtual Gift Card']"
+    recepient_name = 'giftcard_2_RecipientName'
+    recepient_email = 'giftcard_2_RecipientEmail'
+    add_cart_btn = "#add-to-cart-button-2"
+    cart_btn = 'ico-cart'
+    term_condt = 'termsofservice'
+    checkout_btn = 'checkout'
 
 class CheckoutData():
-    login_btn = 'ico-login'
-
+    recepient_name_data = 'steeleangel'
+    recepient_name_email = 'steeleangel00@gmail.com'
+    personal_data_url = 'https://demowebshop.tricentis.com/onepagecheckout'
